f1a3f1a8f790858c0cf7d1af86d84edca23a8862
ByeonSeongUk <seonguk092@gmail.com>
2022-11-30 17:32:10 +0900

    react 기초 학습

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
 
@@ -9,10 +9,11 @@ function App() {
   let post = 'clothes shopping mall';  
 
   // state 사용
-  let [title, titleChange] = useState(['남자 코트 추천', '여자 코트 추천', '아이 코트 추천']);
-  let [likey, likeyCount] = useState([0,0,0]); 
-  let [modal, setModal] = useState(false);
-  let [titleNum, setTitleNum] = useState(0); // 클릭한 게시물 정보 모달창 받아오기
+  let [title, titleChange] = useState(['남자 코트 추천', '여자 코트 추천', '아이 코트 추천']); // 게시물 제목
+  let [likey, likeyCount] = useState([0,0,0]);  // 게시물 좋아요 개수
+  let [modal, setModal] = useState(false); // 모달창 on|off
+  let [titleNum, setTitleNum] = useState(0); // 클릭한 게시물 정보의 seq (모달창 출력을 위해)
+  let [inputTest, setInputTest] = useState(''); // input값 받아오기
 
   [1,2,3].map(function(a){
     return 'map test'
@@ -49,6 +50,23 @@ function App() {
           }}
         > 글정렬 
         </button>
+
+        {/* input 창 테스트 */}
+        <input onChange={(e)=>{
+          setInputTest(e.target.value);
+          console.log(inputTest);
+        }}/>
+
+        <button onClick={()=>{
+          // array, object는 바로 값을 변경하려고 하면 안된다.
+          // 꼭 복사본을 가져온 후 -> 수정 -> 변경 함수에 적용 순서대로 작업
+          let copy = [...title];
+          let copy2 = [...likey];
+          copy.unshift(inputTest);
+          copy2.unshift(0);
+          titleChange(copy);
+          likeyCount(copy2);
+        }}>발행</button>
       </div>
       {/* 
       <div className="list">
@@ -101,6 +119,15 @@ function App() {
                 { likey[i] }
               </h4>
               <p>2월 17일 발행</p>
+
+              {/* 게시물 삭제 버튼 */}
+              <button onClick={()=>{
+                let copy = [...title];
+                let remove = copy.splice(i, 1); // 인덱스 [i] 부터, 요소 [1]개 제거, 삭제한 데이터 [let remove로 반환]
+                console.log('삭제 게시물 : '+remove);
+                titleChange(copy);
+              }}>삭제</button>
+
             </div>
           )
         })
@@ -114,7 +141,9 @@ function App() {
 
       {/* 중괄호 문법 사용(Style) */}
       <h4 style={ {color:'red', fontSize:'16px'} }>{post}</h4>
-    
+      
+      <Modal3></Modal3>
+
     </div>
   );
 }
@@ -133,7 +162,7 @@ function Modal(props) {
 }
 
 /*
-// 컴포넌트를 만드는 방법 1
+// 컴포넌트를 만드는 방법 1 - function 사용
 function Modal1() {
   return (
     <div className="modal">
@@ -144,8 +173,8 @@ function Modal1() {
   );
 }
 
-// 컴포넌트를 만드는 방법 2
-const Modal2 = () => {
+// 컴포넌트를 만드는 방법 2 - 변수, 상수 선언
+const Modal2 = () => { // let, var, const 사용 가능
   return(    
     <div className="modal">
       <h4>제목</h4>
@@ -155,4 +184,27 @@ const Modal2 = () => {
   );
 }
 */
+
+// 컴포넌트를 만드는 방법 3 - CLASS 문법
+class Modal3 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title : '남자 코트 추천',
+      date : '2월 17일',
+      content : '상세내용'
+    }
+  }
+
+  render() {
+    return (
+      <div className="modal">
+        <h4>{this.state.title}</h4>
+        <p>{this.state.date}</p>
+        <p>{this.state.content}</p>
+				<button onClick={()=>{ this.setState({title : '여자 코트 추천'}) }}>수정</button>
+      </div>
+    )
+  }
+}
 export default App;
